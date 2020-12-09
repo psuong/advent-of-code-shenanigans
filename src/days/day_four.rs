7b4d06e430df0fe66f3f3bede1173e9047292f02
Porrith <porrith.suong@gmail.com>
2020-12-08 22:36:45 -0500

    Fixed day 4 needed two masks.

--- src/days/day_four.rs
+++ src/days/day_four.rs
@@ -4,6 +4,7 @@ use regex::Regex;
 const FIELD : [&str; 8] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"];
 const EYE_COLOR : [&str; 7] = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
 const REQUIRED_MASK : i32 = 0b0111_1111;
+const CID_MASK : i32 = 0b1000_0000;
 
 const DELIMITER : char = ' ';
 #[allow(dead_code)]
@@ -23,9 +24,7 @@ fn scan_words(words : &Vec<&str>, mask : &mut i32) {
                 if is_data_valid(&idx, &word) {
                     *mask |= 1 << i;
                     break;
-                } /*else {
-                    println!("Invalid for {}", term);
-                }*/
+                }
             }
         }
     }
@@ -34,8 +33,6 @@ fn scan_words(words : &Vec<&str>, mask : &mut i32) {
 fn is_data_valid(index : &i32, word : &str) -> bool {
     let mut split = word.split(PAIR_DELIMITER).enumerate();
     let (_size, value) = split.nth(1).unwrap();
-    // let idx = *index as usize;
-    // println!("Value at idx {} for {}", FIELD.get(idx).unwrap(), value);
     match index {
         0 => {
             match value.parse::<i32>() {
@@ -56,13 +53,29 @@ fn is_data_valid(index : &i32, word : &str) -> bool {
             }
         },
         3 => {
-           match value.replace(INCH, "").replace(CENTIMETER, "").parse::<i32>() {
-               Ok(n) => return (n >= 150 && n <= 193) || (n >= 59 && n <= 76),
-               Err(_err) => return false
-           }
+            let is_inch = value.contains(INCH);
+            let is_cm = value.contains(CENTIMETER);
+
+            let height = value.replace(INCH, "").replace(CENTIMETER, "").parse::<i32>();
+
+            if is_inch {
+                match height {
+                   Ok(n) => return n >= 59 && n <= 76,
+                   Err(_err) => return false
+                }
+            }
+
+            if is_cm {
+                match height {
+                    Ok(n) => return n >= 150 && n <= 193,
+                    Err(_err) => return false
+                }
+            }
+
+            false
         },
         4 => {
-            let pattern = Regex::new(r"#[a-f0-9]{6}").unwrap();
+            let pattern = Regex::new(r"^#[a-f0-9]{6}").unwrap();
             let is_match = pattern.is_match(value);
             return is_match
         },
@@ -76,11 +89,11 @@ fn is_data_valid(index : &i32, word : &str) -> bool {
             return false
         },
         6 => {
-            let pattern = Regex::new(r"[0-9]{9,}").unwrap();
+            let pattern = Regex::new(r"^[0-9]{9}").unwrap();
             return pattern.is_match(value)
         },
         7 => {
-            true
+           true 
         },
         _ => {
             return false
@@ -101,15 +114,22 @@ pub fn answer_day_four() {
             scan_words(&words, &mut mask);
 
             if line_length == 0 {
-                let min_mask = mask & REQUIRED_MASK;
-                if mask >= REQUIRED_MASK && min_mask == REQUIRED_MASK {
+                let min_mask = mask ^ REQUIRED_MASK;
+                let condition = min_mask == CID_MASK || min_mask == 0;
+
+                // println!("Mask: {:#010b}, REQUIRED: {:#010b} Cond: {}", mask, REQUIRED_MASK, condition);
+                if condition {
                     valid_passports += 1;
                 }
                 mask = 0;
             };
         }
+        
+        let min_mask = mask & REQUIRED_MASK;
+        let condition = min_mask == CID_MASK || min_mask == 0;
 
-        if mask >= REQUIRED_MASK {
+        if condition {
+            // println!("Mask: {:#010b}, REQUIRED: {:#010b} Cond: {}", mask, REQUIRED_MASK, condition);
             valid_passports += 1;
         }
 
